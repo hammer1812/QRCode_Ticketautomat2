318d78b4b839a005941179ec0ab11bcd80089809
GeldJunge <philip-foerster@t-online.de>
2019-03-23 14:09:07 +0100

    rewrote functionality and design of ID handling
    added new class, that stores and operates with values like ID, name, date of purchase etc.
    
    TO-DO: Fix GUI

---
+++ profile.py
@@ -0,0 +1,64 @@
+import pyqrcode
+import random
+from datetime import datetime
+
+
+class Profile:
+    name = " "
+    id = 0
+    dateOfPurchase = " "
+    price = 0
+    idList = []
+    stringID = " "
+    idQR = pyqrcode.QRCode
+
+    def __init__(self, name):
+        self.name = name
+        self.dateOfPurchase = datetime.now()
+        self.__generateID()
+
+    def __del__(self):  # I'm about to end this man's whole career.
+        del self.name
+        del self.id
+        del self.dateOfPurchase
+        del self.price
+        del self.idList
+        del self.stringID
+        del self.idQR
+
+    #  private Methode zum erzeugen der ID und des QRCOdes
+    def __generateID(self):
+        self.id = random.randrange(10000, 99999, 1)
+        with open("idList.txt", "r") as read_list:
+            read_list.seek(0)
+            self.idList = read_list.readlines()
+            while self.id not in self.idList:
+                self.idList.append(self.id)
+                self.stringID += self.name[0]
+                self.stringID += datetime.now()
+                self.stringID += str(id)
+                self.stringID += self.name[-1]
+                with open("idList.txt", "w") as write_list:
+                    write_list.writelines("%s\n" % line for line in self.idList)
+                self.idQR = pyqrcode.create(str(self.id) + "\n" + self.name + "\n" + self.dateOfPurchase, error='L', mode='binary')
+                self.idQR.png('QRCode' + self.name + '.png', scale=5)
+
+    #  die "verschlüsselte" ID in Form eines Strings
+    def getStringID(self):
+        return self.stringID
+
+    #  die ID, wie sie in idList gespeichert ist
+    def getID(self):
+        return self.id
+
+    #  der korrespondierende Name
+    def getName(self):
+        return self.name
+
+    #  das korrespondierende Kaufdatum
+    def getDateOfPurchase(self):
+        return self.dateOfPurchase
+
+    #  das korrespondierende QRCode-Objekt
+    def getQRImage(self):
+        return self.idQR
--- qrcode_ticketautomat.py
+++ qrcode_ticketautomat.py
@@ -1,5 +1,5 @@
 import pyqrcode
-import time
+from datetime import datetime
 import random
 import os
 import wx
@@ -7,107 +7,120 @@ import sys
 import traceback
 import tkinter
 from tkfilebrowser import askopendirname, askopenfilename, asksaveasfilename
+import profile
 
 
+#  Ein Hauch von Tüll. Wohl eher gehört in den Müll!
 class Panels(wx.Panel):
-	def __init__(self, parent, id):
-		wx.Panel.__init__(self, parent)
-		self.parent = parent
+    def __init__(self, parent, id):
+        wx.Panel.__init__(self, parent)
+        self.parent = parent
+
 
 class MainWindow(wx.Frame):
-	def __init__(self, parent, title):
-		super(MainWindow, self).__init__()
-		self.parent = parent
+    def __init__(self, parent, title):
+        super(MainWindow, self).__init__()
+        self.parent = parent
 
-		# self.panel = wx.Panel(self)
-		#
-		# self.vsizer = wx.BoxSizer(wx.VERTICAL)
-		# self.nm = wx.StaticBox(self.panel, wx.ID_ANY, "Name: ")
-		# self.nmSizer = wx.StaticBoxSizer(self.nm, wx.VERTICAL)
-		# self.hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.png = pyqrcode.QRCode
+        self.bitmap = wx.StaticBitmap
+        self.qrprofile = profile.Profile
 
-		topsizer = wx.BoxSizer(wx.VERTICAL)
-		button_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        # self.panel = wx.Panel(self)
+        #
+        # self.vsizer = wx.BoxSizer(wx.VERTICAL)
+        # self.nm = wx.StaticBox(self.panel, wx.ID_ANY, "Name: ")
+        # self.nmSizer = wx.StaticBoxSizer(self.nm, wx.VERTICAL)
+        # self.hsizer = wx.BoxSizer(wx.HORIZONTAL)
 
-		wx.Frame.__init__(self, parent, wx.ID_ANY, title=title, size=(250, 300))
-		self.control = wx.TextCtrl(self, wx.ID_ANY, style=wx.TE_PROCESS_ENTER)
-		self.label = wx.StaticText(self, wx.ID_ANY, style=wx.TE_CENTER)
-		self.label.SetBackgroundColour("white")
-		self.resetIdList = wx.Button(self, wx.ID_ANY, "ID-Liste zurücksetzen")
-		self.loadQR = wx.Button(self, wx.ID_ANY, "QR-Code prüfen")
-		#self.resetIdList.SetPosition((500, 0))
+        topsizer = wx.BoxSizer(wx.VERTICAL)
+        button_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-		topsizer.Add(self.control, 1, wx.EXPAND)
-		# StaticText um DateOfPurchase zu zeigen
-		button_sizer.Add(self.resetIdList, 0, wx.ALL, 10)
-		topsizer.Add(button_sizer, wx.SizerFlags(0).Center())
-		self.SetSizerAndFit(topsizer)
+        wx.Frame.__init__(self, parent, wx.ID_ANY, title=title, size=(250, 300))
+        self.control = wx.TextCtrl(self, wx.ID_ANY, style=wx.TE_PROCESS_ENTER)
+        self.label = wx.StaticText(self, wx.ID_ANY, style=wx.TE_CENTER)
+        self.label.SetBackgroundColour("white")
+        self.resetIdList = wx.Button(self, wx.ID_ANY, "ID-Liste zurücksetzen")
+        self.loadQR = wx.Button(self, wx.ID_ANY, "QR-Code prüfen")
+        # self.resetIdList.SetPosition((500, 0))
+
+        topsizer.Add(self.control, 1, wx.EXPAND)
+        # StaticText um DateOfPurchase zu zeigen
+        button_sizer.Add(self.resetIdList, 0, wx.ALL, 10)
+        topsizer.Add(button_sizer, wx.SizerFlags(0).Center())
+        self.SetSizerAndFit(topsizer)
+
+        # self.vsizer.Add(self.control, 0, 0, 0)
+        # self.vsizer.Add(self.label, 0, 0, 0)
+        # self.hsizer.Add(self.resetIdList, 0, wx.ALIGN_RIGHT)
+        # self.nmSizer.Add(self.vsizer, 0, 0, 0)
+        # self.panel.SetSizer(self.vsizer)
+        self.CreateStatusBar()
+
+        #  Verknüpfen der Buttons mit Funktionen
+        self.Bind(wx.EVT_TEXT_ENTER, self.onEnter, self.control)
+        self.Bind(wx.EVT_BUTTON, self.onClickedReset, self.resetIdList)
+        self.Bind(wx.EVT_BUTTON, self.onClickedLoadQR, self.loadQR)
+
+        self.Show(True)
+
+    #  Event beim klicken des Laden-Buttons: lade QRCode.png
+    def onClickedLoadQR(self, e):
+        file_path = askopenfilename(initialdir="C:\\", initialfile="tmp",
+                                    filetypes=[("Pictures", "*.png"), ("All Files", "*")])
+        print(file_path)
+        self.png = wx.Image(file_path, wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+        self.bitmap = wx.StaticBitmap(self, wx.ID_ANY, self.png, (10, 5),
+                                      (self.png.GetWidth(), self.png.GetHeight()))
+        self.Update()
+
+    # Event beim klicken des Löschen-Buttons: instanz des Objekts löschen
+    def onClickedDelete(self, e):
+        del self.qrprofile
+
+    # Event beim klicken des Reset-Buttons: idList.txt zurücksetzen
+    def onClickedReset(self, e):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        os.remove(dir_path + '\\idList.txt')
+        with open("idList.txt", "w") as reset_list:
+            reset_list.write("")
+
+    #  Event beim Drücken der Enter-Taste: Instanz des Objekts profile erzeugen und QRCode generieren und .png erstellen
+    def onEnter(self, e):
+        name = str(self.control.GetValue())
+        self.qrprofile = profile(name)
+        self.png = self.qrprofile.getQRImage()
+        self.bitmap = wx.StaticBitmap(self, -1, self.png, (10, 5), (self.png.GetWidth(), self.png.GetHeight()))
+        self.bitmap.SetPosition((0, 50))
+        self.Update()
 
 
-		# self.vsizer.Add(self.control, 0, 0, 0)
-		# self.vsizer.Add(self.label, 0, 0, 0)
-		# self.hsizer.Add(self.resetIdList, 0, wx.ALIGN_RIGHT)
-		# self.nmSizer.Add(self.vsizer, 0, 0, 0)
-		# self.panel.SetSizer(self.vsizer)
-		self.CreateStatusBar()
-
-		self.Bind(wx.EVT_TEXT_ENTER, self.OnEnter, self.control)
-		self.Bind(wx.EVT_BUTTON, self.OnClickedReset, self.resetIdList)
-		self.Bind(wx.EVT_BUTTON, self.onClickedLoadQR, self.loadQR)
-
-		self.Show(True)
-
-	def onClickedLoadQR(self, e):
-		file_path = askopenfilename(initialdir="C:\\", initialfile="tmp", filetypes=[("Pictures", "*.png"), ("All Files", "*")])
-		print(file_path)
-		with open(file_path) as load_code:
-			self.png = wx.Image(file_path, wx.BITMAP_TYPE_PNG).ConvertToBitmap()
-			self.bitmap = wx.StaticBitmap(self, wx.ID_ANY, self.png, (10, 5), (self.png.GetWidth(), self.png.GetHeight()))
-			self.Update()
-
-	def OnClickedReset(self, e):# Event beim klicken des Reset-Buttons
-		dir_path = os.path.dirname(os.path.realpath(__file__))
-		os.remove(dir_path + '\\idList.txt')
-		with open("idList.txt", "w") as reset_list:
-			reset_list.write("")
-
-
-
-	def OnEnter(self, e):	#Event beim Drücken der Enter-Taste
-		name = str(self.control.GetValue())
-		self.label.SetLabel(generateID(name))
-		self.png = wx.Image("QRCode" + name + ".png", wx.BITMAP_TYPE_PNG).ConvertToBitmap()
-		self.bitmap = wx.StaticBitmap(self, -1, self.png, (10, 5), (self.png.GetWidth(), self.png.GetHeight()))
-		self.bitmap.SetPosition((0, 50))
-		self.Update()
-
-
-def generateID(name):
-	stringID = ""
-	id = random.randrange(10000, 99999, 1)
-	with open("idList.txt", "r") as read_list:
-		read_list.seek(0)
-		idList = read_list.readlines()
-		while id not in idList:
-			idList.append(id)
-			stringID += name[0]
-			stringID += time.strftime("%d%m%y%H%M")
-			stringID += str(id)
-			stringID += name[-1]
-			with open("idList.txt", "w") as write_list:
-				write_list.writelines("%s\n" % line for line in idList)
-			idQR = pyqrcode.create(str(id) + "\n" + name + "\n" + time.strftime("%d%m%y%H%M"), error='L', mode='binary')
-			idQR.png('QRCode' + name + '.png', scale=5)
-			return stringID
-	return None
+# def generateID(name):
+# 	stringID = ""
+# 	id = random.randrange(10000, 99999, 1)
+# 	with open("idList.txt", "r") as read_list:
+# 		read_list.seek(0)
+# 		idList = read_list.readlines()
+# 		while id not in idList:
+# 			idList.append(id)
+# 			stringID += name[0]
+# 			stringID += datetime.now()
+# 			stringID += str(id)
+# 			stringID += name[-1]
+# 			with open("idList.txt", "w") as write_list:
+# 				write_list.writelines("%s\n" % line for line in idList)
+# 			idQR = pyqrcode.create(str(id) + "\n" + name + "\n" + time.strftime("%d%m%y%H%M"), error='L', mode='binary')
+# 			idQR.png('QRCode' + name + '.png', scale=5)
+# 			return stringID
+# 	return None
 
 
 def main():
-	app = wx.App(False)
-	frame = MainWindow(None, "QRCode Ticketautomat")
-	frame.Show()
-	app.MainLoop()
+    app = wx.App(False)
+    frame = MainWindow(None, "QRCode Ticketautomat")
+    frame.Show()
+    app.MainLoop()
 
 
 if __name__ == '__main__':
-	main()
+    main()
